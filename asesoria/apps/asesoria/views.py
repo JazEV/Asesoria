7310890660f0e9640083e629b8877068436c67bb
jaz <jazmin_ev@outlook.com>
2019-10-21 16:23:21 -0300

    Agregando templates y create

--- asesoria/apps/asesoria/views.py
+++ asesoria/apps/asesoria/views.py
@@ -2,7 +2,24 @@ from rest_framework import viewsets
 from .serializers import (
 	TurnoSerializer,
 )
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import (
+	Turno,
+)
 
 class TurnoViewSet(viewsets.ModelViewSet):
 	queryset = Turno.objects.all()
 	serializer_class = TurnoSerializer
+
+class PreguntaCreate(LoginRequiredMixin, CreateView):
+	model = Turno
+	fields = ['nombre', 'apellido', 'motivo', 'horario', 'notas']
+	template_name = 'template/turno/create.html'
+	success_url = reverse_lazy('turno:index')
+
+class IndexView(LoginRequiredMixin, ListView):
+	model = Turno
+	template_name = 'templates/turno/index.html'
+	context_object_name = 'turnos'
