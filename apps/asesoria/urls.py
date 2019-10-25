dfa1113294ef38cfc0755f05570d28acfea8be31
Thomas <thomasmoranqn@gmail.com>
2019-10-25 14:15:57 -0300

    Arreglando problemas y armando formulario

---
+++ apps/asesoria/urls.py
@@ -0,0 +1,18 @@
+from django.urls import include, path
+from rest_framework import viewsets
+from rest_framework import routers
+from .views import (
+	TurnoViewSet,
+	TurnoCreate,
+	IndexView,
+)
+
+router = routers.DefaultRouter()
+router.register('turno', TurnoViewSet),
+
+
+urlpatterns = [
+	path('', include(router.urls)),
+	path ('turno/create/', TurnoCreate.as_view(), name='turno'),
+	path ('index/', IndexView.as_view(), name='index'),
+]
--- apps/asesoria/views.py
+++ apps/asesoria/views.py
@@ -3,6 +3,7 @@ from .serializers import (
 	TurnoSerializer,
 )
 from django.views.generic.edit import CreateView
+from django.views.generic import TemplateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import (
@@ -13,13 +14,11 @@ class TurnoViewSet(viewsets.ModelViewSet):
 	queryset = Turno.objects.all()
 	serializer_class = TurnoSerializer
 
-class PreguntaCreate(LoginRequiredMixin, CreateView):
+class TurnoCreate(LoginRequiredMixin, CreateView):
 	model = Turno
 	fields = ['nombre', 'apellido', 'motivo', 'horario', 'notas']
 	template_name = 'template/turno/create.html'
 	success_url = reverse_lazy('turno:index')
 
-class IndexView(LoginRequiredMixin, ListView):
-	model = Turno
-	template_name = 'templates/turno/index.html'
-	context_object_name = 'turnos'
+class IndexView(TemplateView):
+	template_name = 'turno/index.html'
